8a0de45b580e70ef2da9a0e0ee52743673d1af0a
Charlot316 <2280583075@qq.com>
2022-08-08 19:05:31 +0800

    360background done

--- src/views/front/Home/index.js
+++ src/views/front/Home/index.js
@@ -1,5 +1,3 @@
-"use strict";
-
 import React, { Component } from "react";
 import {
   Table,
@@ -20,6 +18,9 @@ import {
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
 import Header from "@/components/Home/Header";
 import styles from "./index.module.styl";
+
+import { Pannellum } from "pannellum-react";
+import myImage from "@/assets/太和门.jpg";
 const { Search } = Input;
 class Scene extends Component {
   state = {};
@@ -32,7 +33,21 @@ class Scene extends Component {
           <Header />
         </div>
         <div className={styles.homeSelecter}></div>
-        <div className={styles.homeContent}></div>
+        <div className={styles.homeContent}>
+          <Pannellum
+            style={{ position: "absolute", top: "0", left: "0" }}
+            width="100%"
+            height="100%"
+            image={myImage}
+            pitch={10}
+            yaw={180}
+            hfov={110}
+            autoLoad
+            onLoad={() => {
+              console.log("panorama loaded");
+            }}
+          ></Pannellum>
+        </div>
       </div>
     );
   }
